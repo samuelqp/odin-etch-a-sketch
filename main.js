2500db55e233b0e941349926776db1699ba8d8fc
Samuel Quinteiro Perez <119834862+samuelqp@users.noreply.github.com>
2024-02-26 19:31:55 -0500

    Fix dimensions

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const gridContainer = document.querySelector("#grid");
 const gridWidth = gridContainer.clientWidth;
+const marginSize = 0;
 
 function createGrid(sideLength) {
   for (let i = 0; i < Math.pow(sideLength, 2); i++) {
     const gridBox = document.createElement('div');
     gridBox.id = `box${i}`;
     gridBox.className = 'gridbox';
-    gridBox.style.minWidth = `${gridWidth / sideLength}px`;
+    gridBox.style.margin = `${marginSize}px`;
+
+    maxWidth = gridWidth / sideLength
+    gridBox.style.maxWidth = `${maxWidth}px`;
+    gridBox.style.minWidth = `${maxWidth - (2 * marginSize)}px`;
+
+    gridBox.style.minHeight = gridBox.style.minWidth;
+    gridBox.style.maxHeight = gridBox.style.maxWidth;
     gridContainer.appendChild(gridBox);
   }
 }
